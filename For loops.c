10e97779ae887dc942329ccc2e8e5f88e44dba35
D33-2454-2023 <parsilankekomz@gmail.com>
2025-03-31 15:27:18 +0300

    Add files via upload

---
+++ For loops.c
@@ -0,0 +1,18 @@
+/*
+For loops
+Author:Parsilanke Komoi
+adm: D33-2454-2023
+date: 31-03-2025
+*/
+#include <stdio.h>
+
+int main() {
+   int i=1;
+   //for(start; stop; step)
+   for(i=1; i<=100; i=i+1){
+       
+       
+        printf("number is :%d, and  cube of the %d is:%d\n",i,i, (i*i*i)) ; 
+  }
+    return 0;
+}
